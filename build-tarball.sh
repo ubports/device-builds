509d4ab132bfc534d6398d7f07772541fba501b5
Florian Leeber <flori@bin.org.in>
2019-09-30 21:44:57 +0200

    Support for sparse images from Halium-7.1 builds (#11)

--- build-tarball.sh
+++ build-tarball.sh
@@ -29,6 +29,18 @@ cp $dir/recovery.img $wDir/partitions
 cp -r "$device_dir/ubuntu-overlay/*" $wDir/ || true
 
 mkdir -p $wDir/system/var/lib/lxc/android/
+
+## SPARSE FILE TRANSLATION
+# Needed with Halium-7.1 builds mostly
+fileType=$(file -b0 $dir/system.img)
+if [[ $fileType == "Android sparse image"* ]]; then
+    echo "Converting sparse image to image"
+    mv $dir/system.img $dir/system.sparse.img
+    simg2img $dir/system.sparse.img $dir/system.img
+    e2fsck -fy $dir/system.img >/dev/null
+    resize2fs -p -M $dir/system.img
+fi
+
 cp $dir/system.img $wDir/system/var/lib/lxc/android/
 tar cfJ "$output/device_"$device"_devel.tar.xz" -C $wDir partitions/ system/
 echo "$(date +%Y%m%d)-$RANDOM" > "$output/device_"$device"_devel.tar.build"
